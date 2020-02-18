6fa511b7db0366c2256675fe070a4eae6484ff0d
ulungfikri <ulung.fikri@gmail.com>
2020-02-18 14:02:22 +0700

    Ciller bdg

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -140,6 +140,81 @@ class SiteController extends Controller
         // return $this->render('index');
     }
 
+    public function actionReportcilerbdg29() 
+    {
+        // return $this->render('index');
+
+        // get your HTML raw content without any layouts or scripts
+        $content_value = $this->renderPartial('dks_ciler_value_bdg29');
+        $content_header = $this->renderPartial('dks_ciler_header_bdg29');
+        $content_footer = $this->renderPartial('dks_ciler_footer_bdg29');
+        
+        // setup kartik\mpdf\Pdf component
+        $pdf = new Pdf([
+            // set to use core fonts only
+            'mode' => Pdf::MODE_CORE, 
+            // A4 paper format
+            'format' => Pdf::FORMAT_A4, 
+            // portrait orientation
+            'orientation' => Pdf::ORIENT_LANDSCAPE, 
+            // stream to browser inline
+            'destination' => Pdf::DEST_BROWSER, 
+            // your html content input
+            'content' => $content_value,  
+            // ruler
+            'marginHeader' => 0,  
+            'marginTop' => 74,  
+            'marginFooter' => -7,  
+            'marginBottom' => 26,  
+            'marginLeft' => 9,  
+            'marginRight' => -8,  
+            // 'disableBorderHeader' => true,  
+            // format content from your own css file if needed or use the
+            // enhanced bootstrap css built by Krajee for mPDF formatting 
+            // 'cssFile' => '@vendor/kartik-v/yii2-mpdf/src/assets/custom.css',
+            // any css to be embedded if required
+            // 'cssInline' => '.kv-heading-2{font-size:50px}', 
+            'cssInline' => "
+                .table td 
+                {
+                    text-align: center; 
+                    vertical-align: middle;
+                    white-space: nowrap;
+                }
+                .table_value td
+                {
+                    font-size: 55%;
+                    // white-space: nowrap;
+                    display: inline-block;
+                }
+                .table_catatan, .table_analisa, .table_summary {
+                    page-break-inside: avoid;
+                }
+                .div_table_catatan, .div_table_analisa, .div_table_summary {
+                    // border: 1px solid #4CAF50;
+                    padding-bottom: -10px;
+                }", 
+            // set mPDF properties on the fly
+            'options' => [
+                'title' => 'Krajee Report Title',
+            ],
+            // call mPDF methods on the fly
+            'methods' => [ 
+                // 'SetHeader'=>['CABANG / STOCK POINT  :  FBSLT||'], 
+                // 'SetFooter'=>['FRM-SLS-002.A Rev 04||Last Update 24-12-2018 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Printed By E-FastOne'],
+                // 'SetHTMLFooterByName'=>'<html>asdasdasdasdasd</html>',
+                // 'SetHeader'=>false, 
+                // 'SetFooter'=>false,
+                'SetHeader'=>[$content_header],
+                'SetFooter'=>[$content_footer],
+            ]
+        ]);
+        
+        // return the pdf output as per the destination setting
+        return $pdf->render(); 
+        // return $this->render('index');
+    }
+
     public function behaviors()
     {
         return [
---
+++ frontend/views/site/dks_ciler_value_bdg29.php
@@ -0,0 +1,157 @@
+<div width=97%>
+    <table class="table table_value" cellpadding="1.2" width="100%">
+
+    <!-- ruler -->
+    <tr>
+        <th height="1" width="20"></th> <!--No -->
+       
+
+        <th height="1" width="98"></th>
+       
+
+        <th height="1" width="88"></th>
+       
+        <th height="1" width="98"></th> 
+       
+        <th height="1" width="20"></th> 
+       
+        <th height="1" width="20"></th> 
+       
+    </tr>
+
+
+    <!-- title -->
+    <tr>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" rowspan=2>1</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" width="" rowspan=2>2</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" width="" rowspan=2>3</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" width="" rowspan=2>GPS KOORDINAT</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" rowspan=2 text-rotate="90">Card Flag</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" rowspan=2 text-rotate="90">Profile Class</td>
+        <!-- <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" colspan=7>WAKTU / JAM</td> -->
+        <!-- <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" colspan=20>PRODUCT</td> -->
+        <!-- <td style="border-top: 1px solid ##525252; border-bottom: 1px solid ##525252; border-left: 1px solid ##525252; border-right: 1px solid ##525252" align="center" valign=middle></td> -->
+        <!-- <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" colspan=2>AGEL</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" colspan=2>INA</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" colspan=2>MONY</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000">&nbsp;</td> -->
+        <!-- <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" rowspan=2>JARAK(m)</td> -->
+        <!-- <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" width="" colspan=3 rowspan=2>KETERANGAN</td> -->
+    </tr>
+    <tr>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">Manual / Scanner</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90" width="">TIBA</td>
+        
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" width="">PHOTO TIBA</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">Manual / Scanner</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90" width="">Keluar</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" width="">PHOTO KELUAR</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90" width="">Efektif</td>
+
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">GOOD MOOD</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">MYTEA TEH 350</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">MYTEA TEH 450</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">LAIN - LAIN</td>
+        <!--         <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90"></td> -->
+        <!-- <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">LAIN - LAIN</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">&nbsp;</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">LAIN - LAIN</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">ES MONY</td>
+        <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000" text-rotate="90">LAIN - LAIN</td> -->
+        <!-- <td style="border-top: 1px solid #000000; border-bottom: 1px solid #000000; border-left: 1px solid #000000; border-right: 1px solid #000000">KETERANGAN</td> -->
+    </tr>
+    <!-- isi -->
+    <!-- <tr>
+        <td rowspan=2 height="20" align="center" valign=middle sdval="2" sdnum="1033;"><font color="#000000">02</font></td>
+        <td align="center" valign=middle><font color="#000000">SUCI MAKMUR</font></td>
+        <td colspan=3 align="center" valign=middle><font color="#000000">M JL. SUCI RT 09 / 04 SMI</font></td>
+        <td colspan=3 align="center" valign=middle><font color="#000000">V -6.4663721/106.8540221</font></td>
+        <td rowspan=2 align="center" valign=middle><font color="#000000">Y</font></td>
+        <td rowspan=2 align="center" valign=middle><font color="#000000">GR</font></td>
+        <td rowspan=2 align="center" valign=middle><font color="#000000">S</font></td>
+        <td rowspan=2 align="center" valign=middle sdval="0.447222222222222" sdnum="1033;1033;H:MM"><font color="#000000">10:44</font></td>
+        <td rowspan=2 align="center" valign=middle><font color="#000000">S</font></td>
+        <td rowspan=2 align="center" valign=middle sdval="0.447916666666667" sdnum="1033;1033;H:MM"><font color="#000000">10:45</font></td>
+        <td rowspan=2 align="center" valign=middle sdval="0.000694444444444444" sdnum="1033;1033;H:MM"><font color="#000000">0:01</font></td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle sdval="14" sdnum="1033;"><font color="#000000">20</font></td>
+        <td align="center" valign=middle bgcolor="#000000">&nbsp;</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td align="center" valign=middle>20</td>
+        <td style="border-top: 1px solid #000000" align="center" valign=middle><font color="#000000">BMA</font></td>
+        <td style="border-top: 1px solid #000000" align="center" valign=middle>&nbsp;</td>
+    </tr>
+    <tr>
+        <td align="center" valign=middle><font color="#000000">-6.4662886/106.8562463</font></td>
+        <td colspan=3 align="center" valign=middle>&nbsp;</td>
+        <td colspan=3 align="center" valign=middle><font color="#000000"> /</font></td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle bgcolor="#000000">&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+        <td align="center" valign=middle>&nbsp;</td>
+    </tr> -->
+</table>
